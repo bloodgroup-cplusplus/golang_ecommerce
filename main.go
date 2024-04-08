5360658ef6a0cca7870269f4b4f1d72e589f2922
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2024-04-08 09:48:19 +0530

    sending git commit messages

--- main.go
+++ main.go
@@ -1,6 +1,29 @@
 package main 
 import "fmt"
 
+
+func sendMailSimple() {
+  smtp.PlainAuth(
+    "",
+    "clrsclrsclrsclrsclrs@gmail.com",
+    "",
+    "smtp.gmail.com",
+
+    )
+    
+    msg := "Subject : My special subject \n this is the body"
+    smtp.SendMail(
+    "smtp.gmail.com:587",
+    auth,
+    "clrsclrsclrsclrsclrs@gmail.com",
+    [] string {"clrsclrsclrsclrsclrs@gmail.com"}
+    )
+
+    if err !=nil {
+      fmt.Println(err)
+  }
+}
+
 func main () {
   fmt.Println("Hello World")
 }
